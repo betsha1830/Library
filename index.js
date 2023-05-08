7e26cfb9873dd803f8a39332423b3ea9827233c7
Betsue Weldemariam <btsegay0@gmail.com>
2023-05-08 18:04:16 +0300

    Added a function that adds Books from a field value to myLibrary array

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let myLibrary = []
+
+//  Storing fields as variables
+
+const titleField = document.getElementById("title-field")
+const authorField = document.getElementById("author-field")
+const pagesField = document.getElementById("pages-field")
+const readField = document.getElementById("read-field")
+const addBtn = document.getElementById("add-btn")
+
 function Book(title, author, pages, read) {
 	this.title = title
 	this.author = author
@@ -6,4 +16,21 @@ function Book(title, author, pages, read) {
 	this.info = function () {
 		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
 	}
+}
+
+addBtn.addEventListener("click", () => {
+	addBookToMyLibrary()
+	console.log(myLibrary)
+})
+
+//  Adds a book object to myLibrary array
+function addBookToMyLibrary() {
+	let newBook = new Book(
+		titleField.value,
+		authorField.value,
+		pagesField.value,
+		readField.value
+	)
+	myLibrary.push(newBook)
+	newBook = {}
 }
