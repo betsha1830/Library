8e4e7eb96ffbce3b4ad96648516483f9df5be512
Betsue Weldemariam <btsegay0@gmail.com>
2023-05-10 10:40:10 +0300

    Added display book function and fixed the dom elements that adds new books

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-let myLibrary = []
+let my_library = []
+let count = 0
+let display_count = 1
+let content_count = 0
 
 //  Storing fields as variables
-
 const titleField = document.getElementById("title-field")
 const authorField = document.getElementById("author-field")
 const pagesField = document.getElementById("pages-field")
 const readField = document.getElementById("read-field")
 const addBtn = document.getElementById("add-btn")
+
+// Storing divs as variables
+const mainDiv = document.getElementById("books")
 
 function Book(title, author, pages, read) {
 	this.title = title
@@ -18,19 +23,55 @@ function Book(title, author, pages, read) {
 	}
 }
 
+//  An event to listen when a book is being submitted
 addBtn.addEventListener("click", () => {
 	addBookToMyLibrary()
-	console.log(myLibrary)
+	createBlock()
+	displayBooks()
 })
 
-//  Adds a book object to myLibrary array
+//  Adds a book object to my_library array
 function addBookToMyLibrary() {
-	let newBook = new Book(
+	let new_book = new Book(
 		titleField.value,
 		authorField.value,
 		pagesField.value,
 		readField.value
 	)
-	myLibrary.push(newBook)
-	newBook = {}
+	my_library.push(new_book)
+	new_book = {}
+}
+
+function displayBooks() {
+	for (let book_count = 0; book_count < my_library.length; book_count++) {
+		if (display_count < my_library.length + 1) {
+			document.querySelector(`#book-${display_count} #title`).innerText =
+				my_library[book_count].title
+			document.querySelector(`#book-${display_count} #author`).innerText =
+				my_library[book_count].author
+			document.querySelector(`#book-${display_count} #pages`).innerText =
+				my_library[book_count].pages
+			document.querySelector(`#book-${display_count} #read`).innerText =
+				my_library[book_count].read
+			display_count += 1
+		}
+	}
+}
+
+function createBlock() {
+	for (let book_count = 0; book_count < my_library.length; book_count++) {
+		if (count < my_library.length) {
+			mainDiv.appendChild(
+				document.createElement("div")
+			).id = `book-${(count += 1)}`
+			if (content_count < count) {
+				let block = document.getElementById(`book-${count}`)
+				block.appendChild(document.createElement("div")).id = "title"
+				block.appendChild(document.createElement("div")).id = "author"
+				block.appendChild(document.createElement("div")).id = "pages"
+				block.appendChild(document.createElement("div")).id = "read"
+				content_count++
+			}
+		}
+	}
 }
